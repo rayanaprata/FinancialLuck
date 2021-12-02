8551703fb0989c0a31701d64ae2d07f07d01c282
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-12-02 11:25:05 -0300

    initial commit

---
+++ Financial Luck/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Financial Luck
+//
+//  Created by C94280a on 02/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func handlerButtonDiscover(_ sender: Any) {
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let random = Bool.random()
+        let randomViewController: UIViewController
+        
+        if random {
+            randomViewController = storyBoard.instantiateViewController(withIdentifier: "richViewController") as! RichViewController
+        } else {
+            randomViewController = storyBoard.instantiateViewController(withIdentifier: "poorViewController") as! PoorViewController
+        }
+        
+        self.present(randomViewController, animated: true, completion: nil)
+    }
+    
+}
